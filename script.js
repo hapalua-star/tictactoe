b9fa8384b23e13ae7f07ad8628616d1083129ae6
Lucas Ebbinghaus <lucasebbinghaus2016@gmail.com>
2024-10-17 16:13:28 -0400

    create two players

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+const Player = (name, marker) => {
+    return { name, marker };
+  };
+  
+  const player1 = Player("Alice", "X");
+  const player2 = Player("Bob", "O");
+
+  
+
+
+
+
+
+
+
+
+
+
+const Gameboard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+    return {
+      getBoard: () => board,
+      updateBoard: (index, playerMark) => {
+        if (board[index] === "") {
+          board[index] = playerMark;
+        }
+      }
+    };
+  })();
+  
+
+
+
+const GameController = (() => {
+    let currentPlayer = player1;
+  
+    const playRound = (index) => {
+      Gameboard.updateBoard(index, currentPlayer.marker);
+      switchTurn();
+    };
+  
+    const switchTurn = () => {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+    };
+  
+    return { playRound };
+  })();
+
+  
+  
+  
+  
